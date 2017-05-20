f1891b8ce8c3840ffbb31907ef9945077dfb52a6
Raphael Cohn <raphael.cohn@stormmq.com>
2017-05-20 09:01:12 +0100

    Added a cstrings module. Fixed some warning

--- src/cstrings/mod.rs
+++ src/cstrings/mod.rs
@@ -2,30 +2,10 @@
 // Copyright © 2016 The developers of rust-extra. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/rust-extra/master/COPYRIGHT.
 
 
-#![allow(non_snake_case)]
-#![allow(non_upper_case_globals)]
-#![feature(core_intrinsics)]
-#![feature(associated_consts)]
+use ::libc::c_char;
+use ::libc::strnlen;
+use ::std::mem::transmute;
+use ::std::slice::from_raw_parts;
 
 
-extern crate core;
-
-
-use ::std::fs::Metadata;
-use ::std::fs::Permissions;
-#[cfg(unix)] use ::std::os::unix::fs::PermissionsExt;
-
-
-pub mod arrays;
-pub mod powersOfTwo;
-
-
-include!("do_while_loop.rs");
-include!("likely.rs");
-include!("offsetOf.rs");
-include!("unlikely.rs");
-include!("u4.rs");
-include!("u5.rs");
-include!("u6.rs");
-include!("u31.rs");
-include!("UnixFileSystemPermissionMode.rs");
+include!("cStrFromFixedLengthString.rs");
